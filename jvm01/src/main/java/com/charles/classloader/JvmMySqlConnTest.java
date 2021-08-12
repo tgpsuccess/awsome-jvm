0d4094a75a1f06f355903e3f906562fb6f7c9e36
tgpsuccess <tgpsuccess@163.com>
2021-08-12 22:18:59 +0800

    Modify the annotations

--- jvm01/src/main/java/com/charles/classloader/JvmMySqlConnTest.java
+++ jvm01/src/main/java/com/charles/classloader/JvmMySqlConnTest.java
@@ -8,27 +8,28 @@ import java.util.ServiceLoader;
 
 /**
  * @author chales.tang
- * @title JvmSqlConnectionTest
+ * @title JvmMySqlConnTest
  * @discription Mysql驱动包下Driver驱动类加载测试
  */
-public class JvmSqlConnectionTest {
+public class JvmMySqlConnTest {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        // 设置当前线程的类加载器为扩展类加载器，会导致线程加载不到mysql驱动包下java.sql.Driver的实现类
-//        Thread.currentThread().setContextClassLoader(JvmSqlConnectionTest.class.getClassLoader().getParent());
+        // 设置当前线程的类加载器为 "扩展类加载器" ，会导致线程加载不到mysql驱动包下java.sql.Driver的实现类
+        Thread.currentThread().setContextClassLoader(JvmMySqlConnTest.class.getClassLoader().getParent());
+
         Connection root = DriverManager.getConnection(
                 "jdbc:mysql://127.0.0.1:3306/mysql?characterEncoding=UTF-8",
                 "root", "root");
+
+        // DriverManager 通过SPI机制懒加载mysql驱动包下java.sql.Driver的实现类
+        ServiceLoader<Driver> loadedDrivers = ServiceLoader.load(Driver.class);
+        loadedDrivers.forEach(System.out::println);
 
         // 通过反射机制加载mysql驱动包下java.sql.Driver的实现类com.mysql.jdbc.Driver
         Class<?> aClass = Class.forName("com.mysql.jdbc.Driver");
         System.out.println(aClass.getClassLoader());
         System.out.println(Thread.currentThread().getContextClassLoader());
-
-        // 通过SPI机制懒加载mysql驱动包下java.sql.Driver的实现类
-        ServiceLoader<Driver> loadedDrivers = ServiceLoader.load(Driver.class);
-        loadedDrivers.forEach(System.out::println);
 
         // java.sql.Driver类在rt.jar包，由启动类加载器进行加载，所以它的加载器为null
         System.out.println(ClassLoader.getSystemClassLoader().loadClass("java.sql.Driver").getClassLoader());
